7eacddc536dc2bb0a55a949cab7bae2a7241b1d4
httpllamaz <airriflepellet@gmail.com>
2023-02-19 14:59:17 +0000

    add: Settings manager
    Add a graphical settings manager that can be called from the terminal. .Desktop file coming soon

--- debian-install.sh
+++ debian-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install build-essential libglib2.0-dev cmake pkg-config libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libx11-dev libgtk-3-dev dmenu terminator tint2 picom yaru-theme-gtk vim
+sudo apt-get install build-essential libglib2.0-dev cmake pkg-config libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libx11-dev libgtk-3-dev dmenu terminator tint2 picom yaru-theme-gtk vim python3-tk pavucontrol lxappearance
 cargo build --release
 sudo cp target/release/skyWM /usr/bin/skyWM
+sudo chmod +x /usr/bin/skyWM
 sudo cp extra/orbital.desktop /usr/share/xsessions
 rm -rf ~/.config/orbitalde
 mkdir ~/.config/orbitalde
@@ -12,3 +13,5 @@ rm -rf ~/.config/tint2
 mv extra/tint2 ~/.config
 rm -rf ~/.config/gtk-3.0
 mv extra/gtk-3.0 ~/.config
+sudo cp extra/orbital-settings /usr/bin
+sudo chmod +x /usr/bin/orbital-settings
